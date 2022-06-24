0fbc19c26f7e91a22a3077fe185bc0453e5cfda7
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-06-24 15:47:02 -0600

    add setup script for clangd

--- setup.sh
+++ setup.sh
@@ -19,14 +19,29 @@ setup_vimplug() {
 }
 
 setup_neovim() {
+    NEOVIM_RELEASE=https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
     prevdir=$(pwd)
     cd /tmp
-    NEOVIM_RELEASE=https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
     wget $NEOVIM_RELEASE
     tar xzvf nvim-linux64.tar.gz
     mv ./nvim-linux64 ~/bin/nvim-linux64
     echo "PATH=$PATH:~/bin/nvim-linux64/bin" >> ~/.bash_profile
+    . ~/.bash_profile
     setup_vimplug
+    cd $prevdir
+}
+
+setup_clangd() {
+    CLANGD_RELEASE=https://github.com/clangd/clangd/releases/download/14.0.3/clangd-linux-14.0.3.zip
+    RELEASE_FILE=clangd-linux-14.0.3.zip
+    RELEASE_DIR=clangd_14.0.3
+    prevdir=$(pwd)
+    cd /tmp
+    wget $CLANGD_RELEASE
+    unzip -o $RELEASE_FILE
+    mv $RELEASE_DIR ~/bin/$RELEASE_DIR
+    echo "PATH=$PATH:~/bin/$RELEASE_DIR/bin" >> ~/.bash_profile
+    . ~/.bash_profile
     cd $prevdir
 }
 
@@ -34,3 +49,4 @@ setup_git
 setup_config_symlink
 setup_usr_bin_dir
 setup_neovim
+setup_clangd
