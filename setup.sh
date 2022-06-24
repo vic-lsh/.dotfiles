c2c769a2a999f586f3c26c9b6f323d3e69d181e8
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-06-24 14:41:42 -0600

    update setup script to install stable neovim

--- setup.sh
+++ setup.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
-git config --global user.name vic-shihang-li
-git config --global user.email 40274755+vic-shihang-li@users.noreply.github.com
+setup_git {
+    git config --global user.name vic-shihang-li
+    git config --global user.email 40274755+vic-shihang-li@users.noreply.github.com
+}
 
-# setup symlinks
-ln -nfs $(pwd)/.config ~/.config
+setup_config_symlink {
+    ln -nfs $(pwd)/.config ~/.config
+}
+
+setup_usr_bin_dir {
+    mkdir ~/bin
+}
+
+setup_neovim {
+    prevdir=$(pwd)
+    cd /tmp
+    NEOVIM_RELEASE=https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
+    wget $NEOVIM_RELEASE
+    tar xzvf nvim-linux64.tar.gz
+    mv ./nvim-linux64/bin/nvim ~/bin/nvim
+    cd $prevdir
+}
+
+setup_git
+setup_config_symlink
+setup_usr_bin_dir
+setup_neovim
