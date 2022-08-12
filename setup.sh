8d708c8325de40cafad321f5ef9c7ea6cc193098
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-08-12 15:23:04 +0000

    add rust-analyzer to path

--- setup.sh
+++ setup.sh
@@ -57,6 +57,7 @@ install_rust_analyzer() {
     mkdir -p ~/.local/bin
     curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
     chmod +x ~/.local/bin/rust-analyzer
+    echo "PATH=$PATH:~/.local/bin" >> ~/.bash_profile
     rustup component add rust-src
 }
 
