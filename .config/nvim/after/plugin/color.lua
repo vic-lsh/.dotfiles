99f960141c1d604d0e423c358b436a1236437710
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-12-18 23:44:34 -0500

    nvim: fix width column display issue

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -1,6 +1,6 @@
 vim.opt.termguicolors = false
 
-vim.api.nvim_set_hl(0, 'ColorColumn', {bg='black'})
+vim.api.nvim_set_hl(0, 'ColorColumn', {ctermbg='Black'})
 vim.api.nvim_set_hl(0, 'SignColumn', {})
 vim.api.nvim_set_hl(0, 'Pmenu', {bg='black', fg='white'})
 -- maybe tweak comment's color to something less obstrusive
