bbc44fce198d516ceff82cc429e2a39316b392af
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-12-26 15:47:56 -0700

    nvim: update required lsps

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,9 +3,9 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
   'sumneko_lua',
   'rust_analyzer',
+  'clangd',
 })
 
 local cmp = require('cmp')
