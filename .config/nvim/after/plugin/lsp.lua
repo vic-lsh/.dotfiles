c0bfca4d34bbd6a8d075ee3840968414b1735eb7
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2023-05-26 00:14:21 -0400

    remove unused language

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,11 +3,8 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'sumneko_lua',
   'rust_analyzer',
   'clangd',
-  'omnisharp',
-  'gopls'
 })
 
 local cmp = require('cmp')
