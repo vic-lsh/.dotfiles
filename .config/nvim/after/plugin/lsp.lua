4508ba6d6c2e93b5a465ca303d32fb2c9fc11a37
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2023-12-12 22:03:30 -0800

    nvim: tweak code action hot key

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -60,7 +60,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "ca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
