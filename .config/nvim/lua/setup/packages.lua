dc43ef0bdaea9d3acb28a972191e1dd3ce68f54d
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2024-07-08 11:04:22 -0700

    Revert "nvim: update lsp-zero dependencies"
    This reverts commit 9536fe33a18698bd6ae56e00a37db9449f5be524.

--- .config/nvim/lua/setup/packages.lua
+++ .config/nvim/lua/setup/packages.lua
@@ -75,10 +75,15 @@ return require('lazy').setup({
 
     	  -- Autocompletion
     	  'hrsh7th/nvim-cmp',
+    	  'hrsh7th/cmp-buffer',
+    	  'hrsh7th/cmp-path',
+    	  'saadparwaiz1/cmp_luasnip',
     	  'hrsh7th/cmp-nvim-lsp',
+    	  'hrsh7th/cmp-nvim-lua',
 
     	  -- Snippets
-    	  'L3MON4D3/LuaSnip',
+    	  -- 'L3MON4D3/LuaSnip',
+    	  'rafamadriz/friendly-snippets',
       }
   },
 
