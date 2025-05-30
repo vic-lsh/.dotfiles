31d287ce2b1e3ab8a2de744cfbe4d0043380b3d0
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-05-30 21:30:34 +0000

    nvim: add project local config support

--- .config/nvim/lua/setup/packages.lua
+++ .config/nvim/lua/setup/packages.lua
@@ -66,6 +66,26 @@ return require('lazy').setup({
   'github/copilot.vim',
 
   {
+    "klen/nvim-config-local",
+    config = function()
+      require('config-local').setup {
+        -- Default options (optional)
+
+        -- Config file patterns to load (lua supported)
+        config_files = { ".nvim.lua", ".nvimrc", ".exrc" },
+
+        -- Where the plugin keeps files data
+        hashfile = vim.fn.stdpath("data") .. "/config-local",
+
+        autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+        commands_create = true,     -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalDeny)
+        silent = false,             -- Disable plugin messages (Config loaded/denied)
+        lookup_parents = false,     -- Lookup config files in parent directories
+      }
+    end
+  },
+
+  {
       'VonHeikemen/lsp-zero.nvim',
       dependencies = {
     	  -- LSP Support
