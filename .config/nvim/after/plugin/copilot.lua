0d6f811f3c074436e078f7719881741d9a479ec7
vic-lsh <40274755+vic-shihang-li@users.noreply.github.com>
2025-09-22 18:45:14 +0000

    feat(nvim): add tab-to-complete for copilot

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -1,2 +1,4 @@
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<CR>")',
+  { silent = true, expr = true, script = true })
